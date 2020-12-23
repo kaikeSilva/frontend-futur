cd8d9292d6f982707dd9c6deb5f18b9e729c0732
kaikeSilva <54440964+kaikeSilva@users.noreply.github.com>
2020-12-23 18:19:40 -0300

    erro de tipo inconsistente

--- src/app/validators/equal-confirm.directive.ts
+++ src/app/validators/equal-confirm.directive.ts
@@ -1,6 +1,6 @@
 import { AbstractControl, FormGroup, ValidationErrors, ValidatorFn} from '@angular/forms';
 
-export const equalConfirm: ValidatorFn = (control: FormGroup): ValidationErrors | null => {
+export const equalConfirm: ValidatorFn | any = (control: FormGroup): ValidationErrors | null => {
 
     if ( control.get('password')?.value != control.get('password_confirm')?.value) {
         return {'equalConfirm': true}
