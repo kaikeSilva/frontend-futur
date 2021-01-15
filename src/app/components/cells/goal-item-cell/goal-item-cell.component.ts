f2ace3fd51cd067b1ed54aa5276d58d195402d75
kaikeSilva <54440964+kaikeSilva@users.noreply.github.com>
2021-01-15 15:56:34 -0300

    interface de goalitens do dashboard estavel

--- src/app/components/cells/goal-item-cell/goal-item-cell.component.ts
+++ src/app/components/cells/goal-item-cell/goal-item-cell.component.ts
@@ -1,4 +1,6 @@
-import { Component, Input, OnInit, SimpleChange } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Input, OnInit, Output, QueryList, SimpleChange, ViewChildren } from '@angular/core';
+import { MatIcon } from '@angular/material/icon';
+import { GoalItemService } from 'src/app/goals/goal-item.service';
 import { GoalItem } from 'src/app/models/goal-item';
 
 @Component({
@@ -9,14 +11,57 @@ import { GoalItem } from 'src/app/models/goal-item';
 export class GoalItemCellComponent implements OnInit {
 
   @Input() goalItem!: GoalItem
+  @ViewChildren("bgSlide") bgSlide!:  QueryList<ElementRef<HTMLDivElement>>
+  @Output() ChangeGoalItemStatusEvent = new EventEmitter<number>();
+  
+  _loading: boolean = false
+  set loading(status: boolean) {
+    this._loading = status
+  }
 
-  constructor() { }
+  get loading(): boolean {
+    return this._loading
+  }
+
+  constructor(
+    private _goalItemService: GoalItemService
+  ) { }
 
   ngOnInit(): void {
   }
 
-  done() {
-    // TODO: adicionar ação para item concluido, desfeito e navegar para recurso 
-    console.log("prontp");
+  ngOnChanges(change: SimpleChange): void {
+    if(this.loading) 
+      this.loading = false
   }
+
+  ngAfterViewInit(): void {
+  }
+
+  done(action: string) {
+    // TODO: adicionar ação para item concluido, desfeito e navegar para recurso
+    this.loading = true; 
+    this._goalItemService.update(this.goalItem.id).subscribe(
+      (res) => {
+        this.ChangeGoalItemStatusEvent.emit(this.goalItem.goal_id)
+      }
+    )
+  }
+
+  // implementação de animação retirada
+  // triggerAnimation(action: string) {
+  //   if(action == "done") {
+  //     if(this.bgSlide.first.nativeElement.classList.contains("transition-before"))
+  //         this.bgSlide.first.nativeElement.classList.toggle("transition-before")
+  //     if(!this.bgSlide.first.nativeElement.classList.contains("transition-after"))
+  //       this.bgSlide.first.nativeElement.classList.toggle("transition-after")
+  //   } else if (action == "undone") {
+  //     setTimeout(() => {
+  //       if(this.bgSlide.first.nativeElement.classList.contains("transition-after"))
+  //         this.bgSlide.first.nativeElement.classList.toggle("transition-after")        
+  //     },1000)
+  //     this.bgSlide.first.nativeElement.classList.toggle("transition-before")
+  //   }
+
+  // }
 }
